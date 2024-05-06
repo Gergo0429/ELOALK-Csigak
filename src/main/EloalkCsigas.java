6b0621d4b41448568b71fa3b27cd9c092f802ceb
Gergo0429 <zsigmondgergo@gmail.com>
2024-05-06 11:46:30 +0300

    Project letrehozva

---
+++ src/main/EloalkCsigas.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ *
+ * @author zsigm
+ */
+public class EloalkCsigas {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
